e23101acc9b7ce9010f5e96b4ad5617bdd8c6596
maximilianozurita <maxi.zurita.seb@gmail.com>
2025-05-23 22:52:40 -0300

    Se agrega nuevo controller imagen

---
+++ TP-WebApi-equipo-2A/Controllers/ImagenController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TP_WebApi_equipo_2A.Controllers
+{
+    public class ImagenController : ApiController
+    {
+        // GET: api/Imagen
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Imagen/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Imagen
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Imagen/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Imagen/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
